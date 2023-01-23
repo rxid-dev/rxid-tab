1566f2cb184e684268b05cb00a6146f96b74fa0c
videotutorialdev <12bedevnetwork@gmail.com>
2023-01-24 02:05:34 +0700

    add: Membuat Tampilan Tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
     <div className="container mt-4">
-      <h1 className='mb-0'>React Tab Tutorial</h1>
+      <h1 className="mb-0">React Tab Tutorial</h1>
       <p>Create reusable tab component (rxid-tab) on React App</p>
+
+      <div className="rxid-tab">
+        <div className="tab-header">
+          <nav className="tab-nav">
+            <ul className="tabs">
+              <li className="tab-item">
+                <a href="#" className="tab-link active">
+                  Tab One
+                </a>
+              </li>
+              <li className="tab-item">
+                <a href="#" className="tab-link">
+                  Tab Two
+                </a>
+              </li>
+              <li className="tab-item">
+                <a href="#" className="tab-link">
+                  Tab Three
+                </a>
+              </li>
+              <li className="tab-item">
+                <a href="#" className="tab-link">
+                  Tab Four
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="tab-body">
+          <div className="tab-content">
+            <h4>Hello World</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
