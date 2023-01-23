613b74ed7b25a52844d7f06822be53d9f61bea3e
videotutorialdev <12bedevnetwork@gmail.com>
2023-01-24 01:43:38 +0700

    add: Requirements & Menyiapkan Workspace

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1 className='mb-0'>React Tab Tutorial</h1>
+      <p>Create reusable tab component (rxid-tab) on React App</p>
+    </div>
+  );
+}
+
+export default App;
